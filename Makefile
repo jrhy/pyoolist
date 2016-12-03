1aeb971eaf35e730b0e0e3e02af1e26df1d0cb61
Jeff Rhyason <jeff@rhyason.com>
2016-12-02 23:06:53 -0800

    add README.md.header

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ all: test README.md
 test:
 	python flisttest.py > .test.out
 
-README.md: README.md.template .test.out
-	( cat README.md.template ; \
+README.md: README.md.header .test.out
+	( cat README.md.header ; \
 	    echo '```' ; \
 	    cat .test.out ; \
 	    echo '```' ) \
--- flisttest.py
+++ flisttest.py
@@ -1,51 +1,53 @@
+import json
+
 from flist import flist, fmap
 
 def test(expr, expected):
     actual = eval(expr)
     if not actual == expected:
-        print expr
+        print(expr)
         raise ValueError("test failed; actual:%s, expected:%s" % (actual, expected))
-    print "%s == %s" % (expr, expected)
+    print("%s == %s" % (expr, json.dumps(expected)))
 
 l = flist([1,2])
-print "l = %s" % l
+print("l = flist(%s)" % l)
 test("l.filter(lambda x: x % 2 == 0).size()     ", 1)
 test("l.map(lambda x: x + 2)                    ", [3, 4])
-print ""
+print("")
 
 l = flist([[1,2],[3,4]])
-print "l = %s" % l
+print("l = flist(%s)" % l)
 test("l.flatten()                               ", [1,2,3,4])
-print ""
+print("")
 
 test("flist([1,2]).flatmap(lambda x: [x, x])         ", [1, 1, 2, 2])
 test("flist([1,2]).reduce(lambda a, b: a + b, 0)     ", 3)
 test("flist(['a','b']).reduce(lambda a, b: a + b, '')", "ab")
-print ""
+print("")
 
 m = fmap({'k1':'v1','k2':'v2'})
-print "m = %s" % m
+print("m = fmap(%s)" % m)
 test("sorted(m.keys())                          ", ['k1','k2'])
 test("sorted(m.values())                        ", ['v1','v2'])
 test("sorted(m.items())                         ", [('k1','v1'),('k2','v2')])
 test("sorted(m.map(lambda key, val: key + val)) ", ['k1v1','k2v2'])
-print ""
+print("")
 
-print "list -> map() -> [(key,val)] -> tomap():"
+print("list -> map() -> [(key,val)] -> tomap():")
 test("m.map(lambda key, val: (key, val)).tomap()", m)
-print ""
+print("")
 
-print "An flist should transform map entries to fmaps:"
+print("An flist should transform map entries to fmaps:")
 l = flist([
       {'id':1, 'op':'registration'},
       {'id':2, 'op':'transfer'}])
-print "l = %s" % l
-test("type(l[0])                                ", fmap)
+print("l = flist(%s)" % l)
+test("str(type(l[0]))                           ", "<class 'flist.fmap'>")
 test("l.map(lambda e: sorted(e.map(lambda key, val: '%s=%s'%(key,val))))\n",
         [['id=1','op=registration'],['id=2','op=transfer']])
-print ""
+print("")
 
-print "pluck:"
+print("pluck:")
 test("l.map(lambda x: {'id':x['id'], 'optype':x['op']})\n", 
     [{'id':1, 'optype':'registration'}, {'id':2, 'optype':'transfer'}])
 
